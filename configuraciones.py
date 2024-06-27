6df1f0811a518152df0a93888dabea1f6560a671
javier montoya <montoya.javier.15@hotmail.com>
2024-06-26 19:29:41 -0600

    primer commit

---
+++ configuraciones.py
@@ -0,0 +1,9 @@
+class Configuraciones():
+    #esta clase almacena las configuraciones del juego
+    def __init__(self):
+        #inicializa las configuraciones del juego
+       
+
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
---
+++ nave.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Nave():
+    #sirbe para gestionar el comportamiento de la nave
+    def __init__(self, pantalla):
+        #inicializa la nave y establece su posicion de partida
+        self.pantalla = pantalla
+
+        #carga la imagen de la nave y obtiene su rect
+        self.imagen = pygame.image.load("img/space-ship.png")
+        self.rect = self.imagen.get_rect()
+        self.pantalla_rec = pantalla.get_rect()
+
+        #eempieza cad nueva nave en la parte infierior central de la pantalla
+        self.rect.centerx = self.pantalla_rect.centerx
+        self.rect.bottom = self.pantalla_rect.bottom
+
+    def blitme(self):
+        #dibuja la nave en su posicion actual
+        self.pantalla.blit(self.imagen, self.rect)
+        
